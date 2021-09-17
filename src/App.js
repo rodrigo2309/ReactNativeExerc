cac2e6d4599e0011a49187b4bfa9492db9b34a4e
Rodrigo Mori <drigo_mori@hotmail.com>
2021-09-17 19:33:21 -0300

    exerc1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function App() {
+    //const jsx = <Text>Primeiro Componente 2</Text>
+    return <Text>Primeiro Componente 2</Text>
+}
+
+export default App
